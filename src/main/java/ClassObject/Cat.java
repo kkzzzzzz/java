cdc33bd9e8cbbe44a44199765f93802bacce0797
kkzzzzzz <zhukaidj@gmail.com>
2016-09-09 21:28:24 +0800

    class

---
+++ src/main/java/ClassObject/Cat.java
@@ -0,0 +1,11 @@
+package ClassObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Project: test-jar
+ * Author: Kevin
+ * Date: 16/9/9
+ * Time: 下午8:47
+ */
+public class Cat {
+}
---
+++ src/main/java/aop/spring/advice/Client.java
@@ -0,0 +1,11 @@
+package aop.spring.advice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Project: test-jar
+ * Author: Kevin
+ * Date: 16/8/15
+ * Time: 下午2:39
+ */
+public class Client {
+}
