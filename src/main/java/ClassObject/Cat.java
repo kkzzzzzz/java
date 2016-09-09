3218ea144d3699b3fcda53fa38ccfad4c56c7431
kkzzzzzz <zhukaidj@gmail.com>
2016-09-09 21:29:46 +0800

    class

--- src/main/java/ClassObject/Cat.java
+++ src/main/java/ClassObject/Cat.java
@@ -8,4 +8,30 @@ package ClassObject;
  * Time: 下午8:47
  */
 public class Cat {
+
+    public static int count;
+    public int age;
+    private String name;
+
+    static {
+        count = 0;
+    }
+
+    public Cat(){
+        age = count++;
+        System.out.println("this is class Cat!");
+
+    }
+
+    public void run(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/ClassObject/CatTest.java
+++ src/main/java/ClassObject/CatTest.java
@@ -1,5 +1,8 @@
 package ClassObject;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * Created with IntelliJ IDEA.
  * Project: test-jar
@@ -8,4 +11,47 @@ package ClassObject;
  * Time: 下午8:49
  */
 public class CatTest {
+
+	public static void main(String[] args) {
+		Class c = Cat.class;
+		Field[] declaredFields = c.getDeclaredFields();
+		for (Field field : declaredFields) {
+			System.out.println(field);
+		}
+
+
+        Method[] methods = c.getDeclaredMethods();
+        for (Method method : methods){
+            System.out.println(method);
+        }
+
+
+        try {
+            Cat cat = (Cat) c.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Class catClass = Class.forName("ClassObject.Cat");
+            Field name = catClass.getDeclaredField("name");
+            //设置为可以访问private属性g
+            name.setAccessible(true);
+            Cat cat2 = (Cat) catClass.newInstance();
+            name.set(cat2,"Aristark");
+            System.out.println(cat2.getName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+	}
+
+
 }
