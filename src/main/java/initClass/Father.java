160e9be822a2e42ba1c0903d830228ba441c5c4a
kkzzzzzz <zhukaidj@gmail.com>
2017-03-07 10:17:43 +0800

    test

--- src/main/java/initClass/Father.java
+++ src/main/java/initClass/Father.java
@@ -7,7 +7,7 @@ package initClass;
  * Date: 16/6/17
  * Time: 下午6:07
  */
-public class Father {
+public class Father extends Super {
     public static int m = 33;
     static{
         System.out.println("父类被初始化");
---
+++ src/main/java/initClass/Super.java
@@ -0,0 +1,15 @@
+package initClass;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Project: test-jar
+ * Author: Kevin
+ * Date: 2017/3/6
+ * Time: 下午9:56
+ */
+public class Super {
+    public static int m = 11;
+    static{
+        System.out.println("执行了super类静态语句块");
+    }
+}
